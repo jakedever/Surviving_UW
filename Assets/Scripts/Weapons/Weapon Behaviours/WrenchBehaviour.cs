32d31c00eb7fea411aa980d06f61c7911adcaf4f
oshawa2 <omarshawa2@gmail.com>
2026-04-01 17:58:44 -0400

    Added Wrench

---
+++ Assets/Scripts/Weapons/Weapon Behaviours/WrenchBehaviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WrenchBehaviour : ProjectileWeaponBehaviour
+{
+   // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * currentSpeed * direction;
+    }
+}
